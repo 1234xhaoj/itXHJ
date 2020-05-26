4141fa5bc4068d37f3739fb8690fec0b18f7691f
1234xhaoj <63988222+1234xhaoj@users.noreply.github.com>
2020-05-26 22:26:39 +0800

    1

---
+++ chap1/enroll.js
@@ -0,0 +1,7 @@
+var enroll = document.getElementById("enroll");
+enroll.onclick = function(e){
+	e.preventDefault();
+	enroll.innerHTML = "附件：2015年卫生管理系学生会名单";
+	enroll.style.background = "#27cb8b";
+	enroll.style.borderColor = "#27cb8b";
+}
